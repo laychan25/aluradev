e5aa1f4d6d038e1cbf33ca66f897c4f31a6c50d4
laychan25 <lauriesilva0@gmail.com>
2025-02-20 19:49:52 -0300

    highLight text area pronto

--- src/components/campo/index.js
+++ src/components/campo/index.js
@@ -1,47 +1,68 @@
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useCor } from "../cotextoCor";
 import style from "./campo.module.scss";
-import hljs from 'highlight.js';
-import 'highlight.js/styles/github.css';
+import hljs from "highlight.js";
+import "highlight.js/styles/atom-one-dark.css";
 
 
+
+
+
+
+
+console.log('hjls importado', hljs)
 
 function Campo() {
   const [code, setCode] = useState("");
-  const [highlightCode , setHightLight] = useState(null)
+  const [highlightCode, setHightLight] = useState(null);
+  const [ativo, setAtivo] = useState(true);
+
+  const toggleElemento = () => {
+    setAtivo((prevState) => !prevState);
+  };
 
   const { cor } = useCor();
 
- useEffect(()=>{
-    hljs.highlightAll()
- },[highlightCode])
+  useEffect(() => {
+    if(!ativo){
+      hljs.highlightAll()
+    }
+  }, [ativo]);
 
-  const ativaHl = ({code}) => {
-    if(code){
+  const ativaHl = ({ code }) => {
+    if (code) {
       const codigoString = code.toString();
-      
-      setHightLight(codigoString)
-      console.log(codigoString)
+      setHightLight(codigoString);
       return <p>{codigoString}</p>;
     }
   };
-
+  
   return (
     <div className={style.container}>
       <div style={{ backgroundColor: `${cor}` }} className={style.cor}>
-        <textarea
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-          type="text"
-          className={style.inputTexto}
-        ></textarea>
+        {ativo ? (
+          <textarea
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            type="text"
+            className={style.inputTexto}
+          >
+            {code}
+          </textarea>
+        ) : (
+          <pre>
+            <code className={style.code}>{highlightCode}</code>
+          </pre>
+        )}
       </div>
-      <pre>
-       <code>{highlightCode}</code>
-      </pre>
       <div className={style.containerBotao}>
-        <button onClick={()=> ativaHl({code})} className={style.botao}>
-          Visualizar com o highlight
+        <button
+          onClick={() => {
+            ativaHl({ code }, toggleElemento());
+          }}
+          className={style.botao}
+        > {ativo ? <p>Vizualizar com highlight</p> : <p>Voltar a editar</p>}
+          
         </button>
       </div>
     </div>
